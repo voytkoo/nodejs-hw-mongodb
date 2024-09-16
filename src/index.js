220bc468f96403a930af6f59f1f187e4fd77e94d
olena <avoytko86@gmail.com>
2024-09-17 01:35:40 +0300

    Зберігаю поточні зміни

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
+};
+bootstrap();
