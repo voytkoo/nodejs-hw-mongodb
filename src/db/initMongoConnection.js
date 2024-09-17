7ecfcfa8e4088611b2e634377d3ac8ddd7beb690
olena <avoytko86@gmail.com>
2024-09-18 00:12:21 +0300

    change mongo items

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,7 +9,7 @@ export const initMongoConnection = async () => {
     const db = env('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=MongoDbCluster`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=ClusterMongo`,
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
