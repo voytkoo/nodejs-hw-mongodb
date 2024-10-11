c92435dba2d56a62ed90bc16d01a4fcd850a2b66
olena <avoytko86@gmail.com>
2024-10-12 00:12:10 +0300

    correct user route in auth

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,7 +1,7 @@
 import createHttpError from 'http-errors';
 
-import { User } from '../db/models/user.js';
 import { Session } from '../db/models/session.js';
+import { User } from '../db/models/user.js';
 
 export const authenticate = async (req, res, next) => {
   const authHeader = req.get('Authorization');
