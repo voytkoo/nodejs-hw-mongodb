5746d4cb31e4217971deb86c6823ecb4a7b6ff69
olena <avoytko86@gmail.com>
2024-10-17 21:01:27 +0300

    update req by id

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -21,6 +21,7 @@ export const getContactsController = async (req, res) => {
     sortBy,
     sortOrder,
     filter,
+    userId: req.user._id,
   });
 
   res.status(200).json({
@@ -33,7 +34,7 @@ export const getContactsController = async (req, res) => {
 export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
 
-  const contact = await getContactById(contactId);
+  const contact = await getContactById(contactId, req.user._id);
 
   if (!contact) {
     throw createHttpError(404, 'Contact not found');
@@ -56,6 +57,7 @@ export const createContactController = async (req, res) => {
     email,
     isFavourite,
     contactType,
+    userId: req.user._id,
   });
 
   res.status(201).json({
@@ -67,8 +69,8 @@ export const createContactController = async (req, res) => {
 
 export const patchContactController = async (req, res, next) => {
   const { contactId } = req.params;
-  const { body } = req;
-  const updatedContact = await updateContact(contactId, body);
+  const updatedContact = await updateContact(contactId, req.user._id, req.body);
+
   if (!updatedContact) {
     throw createHttpError(404, 'Contact not found');
   }
